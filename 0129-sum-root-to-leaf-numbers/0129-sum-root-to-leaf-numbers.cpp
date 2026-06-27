4f15d07296b922f781eb3da2975312166dc95f1f
X •  S2zxx0zxx <236218062+S2zxx0zxx@users.noreply.github.com>
2026-06-27 11:55:30 +0530

    Time: 0 ms (100%), Space: 11.7 MB (90.51%) - LeetHub

---
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int sumNumbers(TreeNode* root) {
+        return dfs(root, 0);
+    }
+    
+private:
+    int dfs(TreeNode* node, int curr) {
+        if (!node) return 0;
+        curr = curr * 10 + node->val;
+        if (!node->left && !node->right) return curr;
+        return dfs(node->left, curr) + dfs(node->right, curr);
+    }
+};
