087829db26aa0701e1c6e108d0f0ab48ef130c4d
X •  S2zxx0zxx <236218062+S2zxx0zxx@users.noreply.github.com>
2026-06-27 11:57:25 +0530

    Time: 9 ms (8.48%), Space: 31.9 MB (94.15%) - LeetHub

---
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.cpp
@@ -0,0 +1,27 @@
+class BSTIterator {
+public:
+    stack<TreeNode*> st;
+    
+    BSTIterator(TreeNode* root) {
+        pushLeft(root);
+    }
+    
+    int next() {
+        TreeNode* node = st.top();
+        st.pop();
+        pushLeft(node->right);
+        return node->val;
+    }
+    
+    bool hasNext() {
+        return !st.empty();
+    }
+    
+private:
+    void pushLeft(TreeNode* node) {
+        while (node) {
+            st.push(node);
+            node = node->left;
+        }
+    }
+};
