da81415e5c8cb7f40a6b3f445625a98e194c7aad
X •  S2zxx0zxx <236218062+S2zxx0zxx@users.noreply.github.com>
2026-06-27 19:26:33 +0530

    Time: 0 ms (100%), Space: 14.8 MB (75.39%) - LeetHub

---
+++ 0086-partition-list/0086-partition-list.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode lessHead(0), greaterHead(0);
+        ListNode* less = &lessHead;
+        ListNode* greater = &greaterHead;
+        
+        while (head) {
+            if (head->val < x) {
+                less->next = head;
+                less = less->next;
+            } else {
+                greater->next = head;
+                greater = greater->next;
+            }
+            head = head->next;
+        }
+        
+        greater->next = nullptr;
+        less->next = greaterHead.next;
+        
+        return lessHead.next;
+    }
+};
