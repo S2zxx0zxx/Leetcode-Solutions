86fd47089a45be261b5f1c7f9ac8178725f8160c
X •  S2zxx0zxx <236218062+S2zxx0zxx@users.noreply.github.com>
2026-06-27 19:10:03 +0530

    Time: 44 ms (48.16%), Space: 152.1 MB (12.09%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.cpp
@@ -0,0 +1,23 @@
+class MinStack {
+public:
+    stack<pair<int,int>> st; // {val, currentMin}
+    
+    MinStack() {}
+    
+    void push(int val) {
+        int minVal = st.empty() ? val : min(val, st.top().second);
+        st.push({val, minVal});
+    }
+    
+    void pop() {
+        st.pop();
+    }
+    
+    int top() {
+        return st.top().first;
+    }
+    
+    int getMin() {
+        return st.top().second;
+    }
+};
