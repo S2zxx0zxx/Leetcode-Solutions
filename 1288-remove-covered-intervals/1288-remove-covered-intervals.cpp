dd361bf5963e617e215787dad5e6ccf2ff34216e
X •  S2zxx0zxx <236218062+S2zxx0zxx@users.noreply.github.com>
2026-07-06 10:15:04 +0530

    Time: 3 ms (42.52%), Space: 15.2 MB (60.13%) - LeetHub

---
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b) {
+            if (a[0] != b[0]) return a[0] < b[0];
+            return a[1] > b[1];
+        });
+        int count = 0, maxEnd = 0;
+        for (auto& iv : intervals) {
+            if (iv[1] > maxEnd) {
+                count++;
+                maxEnd = iv[1];
+            }
+        }
+        return count;
+    }
+};
